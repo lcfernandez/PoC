7541aded53c31c0f5e60ecd6858bc82745108127
lcfernandez <lcfernandez@cos.ufrj.br>
2024-12-08 01:31:36 -0300

    feat: implement static form

--- frontend/src/components/Body/FormInstituicao/index.jsx
+++ frontend/src/components/Body/FormInstituicao/index.jsx
@@ -1,6 +1,15 @@
 const FormInstituicao = () => {
     return (
-        <span>Crie o form aqui</span>
+        <form>
+            <label for="nome">Nome</label>
+            <input type="text" id="nome" name="nome" required="required" />
+            
+            <label for="uf">UF</label>
+            <input type="text" id="uf" name="uf" maxLength="2" required="required" />
+            
+            <label for="qtd_alunos">Quantidade de alunos</label>
+            <input type="number" id="qtd_alunos" name="qtd_alunos" min="0" required="required" />
+        </form>
     );
 }
 
