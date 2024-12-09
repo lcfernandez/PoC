136d7f2cf16e283c80c98689a9e4e5994dca6375
lcfernandez <lcfernandez@cos.ufrj.br>
2024-12-08 23:43:45 -0300

    feat: get institution list dynamically

--- frontend/src/components/Body/InstituicoesTable/index.jsx
+++ frontend/src/components/Body/InstituicoesTable/index.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTable } from 'react-table';
 import './index.css';
+import backendUrl from '../../../utils/backend-url';
+import axios from 'axios';
 
 const InstituicoesTable = () => {
-    const data = React.useMemo(
-        () => [
-            { nome: 'Instituição A', uf: 'SP', qtdAlunos: 1200 },
-            { nome: 'Instituição B', uf: 'RJ', qtdAlunos: 800 },
-            { nome: 'Instituição C', uf: 'MG', qtdAlunos: 950 },
-        ],
-        []
-    );
+    // Estado para a listagem de Instituições
+    const [data, setData] = useState([]);
+
+    // Evento para capturar os registros
+    const fetchInstituicaoList = React.useMemo(() => async () => {
+        try {
+          const res = await axios.get(`${backendUrl}/instituicoes`);
+          setData(res.data);
+        } catch (error) {
+          console.error(error);
+        }
+    }, []);
+    
+    // Inicialização/atualização da listagem
+    useEffect(() => {
+        fetchInstituicaoList();
+    }, []);
 
     const columns = React.useMemo(
         () => [
@@ -34,7 +45,8 @@ const InstituicoesTable = () => {
             <table {...getTableProps()} style={{ width: '100%', maxHeight: '400px', overflowY: 'auto' }}>
                 <thead>
                     {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                        // Como o id nesse objeto é inexistente e só é necessário um, estou colocando estaticamente para evitar o Warning
+                        <tr {...headerGroup.getHeaderGroupProps()} key={1}>
                             {headerGroup.headers.map(column => (
                                 <th {...column.getHeaderProps()} key={column.id}>
                                     {column.render('Header')}
@@ -44,7 +56,7 @@ const InstituicoesTable = () => {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map(row => {
+                    {data.length === 0 ? <tr><td colSpan="3">Não há registros de Instituições.</td></tr> : rows.map(row => { // Tratamento para banco vazio adicionado
                         prepareRow(row);
                         return (
                             <tr {...row.getRowProps()} key={row.id}>
