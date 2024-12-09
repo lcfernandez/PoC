8f7cf7238c498da6bf172b9189c7ad0f16c22fc4
lcfernandez <lcfernandez@cos.ufrj.br>
2024-12-09 12:45:03 -0300

    feat: delete institution by id

--- backend/src/routers/instituicoes.js
+++ backend/src/routers/instituicoes.js
@@ -1,3 +1,5 @@
+const { default: mongoose } = require('mongoose')
+
 // Rotas para o CRUD de Instituições
 const express = require('express')
 const router = new express.Router()
@@ -17,6 +19,39 @@ router.post('/', async (req, res) => {
     try {
         await Instituicoes.create(req.body);
         res.sendStatus(201);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+})
+
+// Remove uma Instituição
+router.delete('/:id', async (req, res) => {
+
+    const { id } = req.params;
+    const institutionId = mongoose.Types.ObjectId.createFromHexString(id);
+
+    try {
+
+        // Verifica existência da Instituição pelo id
+        const institution = await Instituicoes.findOne(
+            {
+                _id: institutionId
+            }
+        );
+
+        if (institution) {
+            await Instituicoes.deleteOne(
+                {
+                    _id: institution._id
+                }
+            );
+
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
