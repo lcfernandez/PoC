d5318876c0fc6be11dc2dbba2c3b4484e5d646fa
lcfernandez <lcfernandez@cos.ufrj.br>
2024-12-11 22:41:21 -0300

    feat: implement backend validations

--- backend/src/routers/instituicoes.js
+++ backend/src/routers/instituicoes.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require('mongoose')
+const Joi = require('joi')
 
 // Rotas para o CRUD de Instituições
 const express = require('express')
@@ -21,8 +22,39 @@ router.get('/chart', async (req, res) => {
 
 })
 
+// Schema para validação dos dados de Instituição
+const instituicaoSchema = Joi.object({
+    nome: Joi.string().min(1).required().error(new Error('O Nome é inválido.')),
+    uf: Joi.string().min(2).max(2).required().valid(
+        'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MS', 'MT', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+    ).error(new Error('A UF é inválida.')),
+    qtdAlunos: Joi.number().integer().min(0).required().error(new Error('A Quantidade de Alunos é inválida.')),
+});
+
 // Insere uma Instituição
 router.post('/', async (req, res) => {
+    const instituicao = req.body;
+
+    // Validação dos dados da Instituição
+    const validation = instituicaoSchema.validate(instituicao);
+
+    if (validation.error) {
+        return res.status(422).send(validation.error.message);
+    }
+
+    // Verifica a existência de uma Instituição com o mesmo Nome
+    const instituicaoNome = await Instituicoes.findOne(
+        {
+            nome: req.body.nome
+        }
+    );
+
+    if (instituicaoNome) {
+        // Verifica se a Instituição de mesmo Nome é da mesma UF
+        if (instituicaoNome.uf === req.body.uf) {
+            return res.status(409).send('Já existe uma Instituição com o mesmo Nome e UF.');
+        }
+    }
 
     try {
         await Instituicoes.create(req.body);
@@ -49,6 +81,31 @@ router.put('/:id', async (req, res) => {
         );
 
         if (instituicao) {
+            const instituicaoEdit = req.body;
+
+            // Validação dos novos dados da Instituição
+            const validation = instituicaoSchema.validate(instituicaoEdit);
+        
+            if (validation.error) {
+                console.log(validation)
+                console.log(validation.error)
+                return res.status(422).send(validation.error.message);
+            }
+
+            // Verifica a existência de uma Instituição com o mesmo Nome
+            const instituicaoNome = await Instituicoes.findOne(
+                {
+                    nome: req.body.nome
+                }
+            );
+
+            if (instituicaoNome) {
+                // Verifica se a Instituição de mesmo Nome é da mesma UF
+                if (instituicaoNome.uf === req.body.uf) {
+                    return res.status(409).send('Já existe uma Instituição com o mesmo Nome e UF.');
+                }
+            }
+
             await Instituicoes.updateOne(
                 {
                     _id: instituicao._id
